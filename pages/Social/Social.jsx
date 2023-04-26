c17414950d5f5fef9067339c9042a1159a570a16
krishn-Singh <krishnavirsingh29@gmail.com>
2023-04-26 10:25:50 +0530

    template

---
+++ pages/Social/Social.jsx
@@ -0,0 +1,7 @@
+export default function Social() {
+  return (
+    <div id="Social" className="Social">
+      <h1>Social</h1>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+// pages/_app.js
+import "../styles/global.css";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Next.js Template</title>
+        <link
+          id="external-css"
+          rel="stylesheet"
+          type="text/css"
+          href="https://fonts.googleapis.com/css?family=Quicksand&amp;display=swap"
+          media="all"
+        />
+        <link
+          id="external-css"
+          rel="stylesheet"
+          type="text/css"
+          href="https://fonts.googleapis.com/css?family=Poppins&amp;display=swap"
+          media="all"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
